0449194a8f9881dce9a50c61de6c752bff10e5ff
LuiScolari <luiscolari889@hotmail.com>
2021-10-01 03:05:08 -0300

    adding mongoose and models

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-const express = require('express')
-const app = express()
-const path = require('path')
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose')
+const Campground = require('./models/campground')
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+})
 
 app.get('/', (req, res) => {
     res.render('home')
+});
+
+app.get('/makecampground', async (req, res) => {
+    const myCamp = new Campground({ title: 'Mybackyard', description: 'A really bad campground' })
+    await myCamp.save()
+    res.send(myCamp)
 })
 
 app.listen(3000, () => {
     console.log('Listening to port 3000')
-})
+});
---
+++ models/campground.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const CampgroundSchema = new Schema({
+    title: {
+        type: String,
+    },
+    price:  {
+        type: String,
+    },
+    description: {
+        type: String,
+    },
+    location: {
+        type: String,
+    }
+});
+
+module.exports = mongoose.model('Campground', CampgroundSchema)

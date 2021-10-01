86b5a95d96e6b4d27225c18a6c005692c50a7360
LuiScolari <luiscolari889@hotmail.com>
2021-10-01 05:02:49 -0300

    Starting CRUD

--- app.js
+++ app.js
@@ -19,11 +19,12 @@ app.get('/', (req, res) => {
     res.render('home')
 });
 
-app.get('/makecampground', async (req, res) => {
-    const myCamp = new Campground({ title: 'Mybackyard', description: 'A really bad campground' })
-    await myCamp.save()
-    res.send(myCamp)
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds  = await Campground.find({})
+    res.render('campgrounds/index', {campgrounds})
 })
+
+
 
 app.listen(3000, () => {
     console.log('Listening to port 3000')

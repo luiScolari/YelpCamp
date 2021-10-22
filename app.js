1d9eb1f8b8b35ebdd669abdf885b0a4f04d0566f
luiScolari <luiscolari889@hotmail.com>
2021-10-22 01:09:38 -0300

    AppError class and async errors

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const Campground = require('./models/campground');
 const methodOverride = require('method-override');
 const { application } = require('express');
 const { findByIdAndUpdate, findById } = require('./models/campground');
+const AppError = require('./utils/AppError');
+const wrapAsync = require('./utils/asynCatch')
 
 app.engine('ejs', ejsMate)
 
@@ -28,44 +30,48 @@ app.get('/', (req, res) => {
     res.render('home')
 });
 
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', wrapAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds });
-})
+}));
 
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 });
 
-app.post('/campgrounds', (req, res) => {
+app.post('/campgrounds', wrapAsync(async (req, res) => {
     const newCamp = new Campground(req.body.campground);
-    newCamp.save();
+    await newCamp.save();
     res.redirect(`/campgrounds/${newCamp._id}`);
-});
+}));
 
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', wrapAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
-});
+}));
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', wrapAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit', { campground });
-})
+}));
 
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', wrapAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`);
-})
+}));
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', wrapAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
-})
+}));
 
+app.use((err, req, res, next) => {
+    const { message = 'Something went wrong', status = 500 } = err;
+    res.status(status).send(message)
+})
 
 app.listen(3000, () => {
     console.log('Listening to port 3000');
--- models/campground.js
+++ models/campground.js
@@ -4,18 +4,23 @@ const Schema = mongoose.Schema
 const CampgroundSchema = new Schema({
     title: {
         type: String,
+        required: true,
     },
     price: {
         type: Number,
+        required: true,
     },
     description: {
         type: String,
+        required: true,
     },
     location: {
         type: String,
+        required: true,
     },
     image: {
         type: String,
+        required: true,
     }
 });
 
